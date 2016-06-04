a7f8b3194e5cdda4d88dff91580121f2701a64ad
Richard Warburton <richard.warburton@gmail.com>
2016-06-04 09:39:07 +0100

    bench-2

--- src/main/java/com/iteratrlearning/collections/performance/HashMapGetBenchmark.java
+++ src/main/java/com/iteratrlearning/collections/performance/HashMapGetBenchmark.java
@@ -17,18 +17,18 @@ import static org.openjdk.jmh.annotations.Mode.AverageTime;
 public class HashMapGetBenchmark
 {
 
-    @Param({"10", "10000", "1000000"})
-    //@Param({"1000"})
+    //@Param({"10", "10000", "1000000"})
+    @Param({"10000"})
     int size;
 
-    @Param({"0.1", /*"0.5", "0.9"*/})
+    @Param({/*"0.1"*/, "0.5", /*"0.9"*/})
     double collisionProb;
 
-    @Param({"JdkMap", "Koloboke" })
+    @Param({"JdkMap"/*, "Koloboke"*/ })
     String mapType;
 
-    //@Param({"Comparable", "InComparable"})
-    @Param({"Comparable"})
+    @Param({"Comparable", "InComparable"})
+    //@Param({"Comparable"})
     String keyType;
 
     Map<Object, String> map;
